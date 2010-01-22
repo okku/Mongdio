ba9425ef3d0e785ac22634fe7742893771a63971
Okku Touronen <theokku@gmail.com>
2010-01-22 16:58:12 +0100

    Init

---
+++ Mongdio/MainForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Mongdio
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.treeViewMenu = new System.Windows.Forms.TreeView();
+			this.editor1 = new Mongdio.MongoEditorControl();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(876, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Location = new System.Drawing.Point(0, 594);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(876, 22);
+			this.statusStrip1.TabIndex = 1;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.treeViewMenu);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.editor1);
+			this.splitContainer1.Size = new System.Drawing.Size(876, 570);
+			this.splitContainer1.SplitterDistance = 177;
+			this.splitContainer1.TabIndex = 2;
+			// 
+			// treeViewMenu
+			// 
+			this.treeViewMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.treeViewMenu.Location = new System.Drawing.Point(0, 0);
+			this.treeViewMenu.Name = "treeViewMenu";
+			this.treeViewMenu.Size = new System.Drawing.Size(177, 570);
+			this.treeViewMenu.TabIndex = 0;
+			// 
+			// editor1
+			// 
+			this.editor1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.editor1.Location = new System.Drawing.Point(0, 0);
+			this.editor1.Name = "editor1";
+			this.editor1.Size = new System.Drawing.Size(695, 570);
+			this.editor1.TabIndex = 0;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(876, 616);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MainForm";
+			this.Text = "Mongdio";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TreeView treeViewMenu;
+		private MongoEditorControl editor1;
+	}
+}
+
---
+++ Mongdio/MongoEditorControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MongdioLogic;
+
+namespace Mongdio
+{
+	public partial class MongoEditorControl : UserControl
+	{
+		private MongoEditorSession _session;
+
+		public MongoEditorControl()
+		{
+			InitializeComponent();
+			KeyDown += MongoEditorControl_KeyDown;
+			rtEditor.KeyDown += MongoEditorControl_KeyDown;
+
+			_session = new MongoEditorSession();
+		}
+
+		void MongoEditorControl_KeyDown(object sender, KeyEventArgs e)
+		{
+			if(e.Alt && e.KeyCode == Keys.X)
+			{
+				//MessageBox.Show(this, rtEditor.SelectedText);
+				var result = _session.Execute(rtEditor.SelectedText);
+				rtResult.AppendText(result);
+			}
+		}
+	}
+}
